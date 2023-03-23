aab6504981049058fe3318b6f4c43b76ec216c2e
patal <patourasalexandros@gmail.com>
2023-03-24 01:24:41 +0200

    terminal settings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 --vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files hidden=true<cr>', {desc = "Telescope"})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Live Grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Bufs"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Help Tags"})
+
