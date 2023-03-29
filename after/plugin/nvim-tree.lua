56fa6ce45e0ac6e6954bb6c61284fa0347ebf2c6
patal <patourasalexandros@gmail.com>
2023-03-29 21:59:15 +0300

    fixes 4 nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -7,9 +7,6 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
@@ -17,9 +14,21 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
+	update_focused_file = { enable = true },
+	hijack_directories = {
+		auto_open = false
+	}
+
 })
 
 
-vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>',{})
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle <cr>',{})
+
+-- close nvim if only tree is remains open. Contributors are refusing to this thing...
+-- https://github.com/nvim-tree/nvim-tree.lua/issues/1005#issuecomment-1166393398
+vim.api.nvim_create_autocmd('BufEnter', {
+    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+    nested = true,
+})
