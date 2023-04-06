0c72d27ed77be6c501df0007c386ea9095e13e29
patal <patourasalexandros@gmail.com>
2023-04-06 13:24:50 +0300

    test push new url

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- based on https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/
+-- based on https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim   /
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 require('user.editor')
